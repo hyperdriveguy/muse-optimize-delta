2280b3e2931efe5acac47a6f090712865f7755ca
Hyperdriveguy <hyperdriveguy@gmail.com>
2021-07-03 20:02:12 -0700

    add read_file function

--- optimize.py
+++ optimize.py
@@ -2,16 +2,29 @@
 import argparse
 
 
-def main();
-    pass
+def main():
+    args = get_args()
+    input_file = read_file(args.asm)
 
 
 def get_args():
-    parser = argparse.ArgumentParser(
-        description='A script for optimizing pokecrystal music.')
+    parser = argparse.ArgumentParser(description='A script for optimizing pokecrystal music.')
     parser.add_argument('asm', help='script input file')
     parser.add_argument('output', help='optimized output file')
     return parser.parse_args()
+
+
+def read_file(filename):
+    try:
+        with open(filename, 'r') as input_file:
+            file_contents = tuple(input_file.readlines())
+        return file_contents
+    except FileNotFoundError:
+        print(f'Input file {filename} does not exist.')
+    except PermissionError:
+        print(f'No permission to read file {filename}.')
+    
+    return ()
 
 
 if __name__ == '__main__':
