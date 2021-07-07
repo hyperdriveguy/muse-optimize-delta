c86b750db820b23429845c3310d73fd64cbb92ac
Hyperdriveguy <hyperdriveguy@gmail.com>
2021-07-06 19:05:59 -0700

    Added functions to calculate the amount of bytes each command in the file will take

---
+++ command_to_byte_size.md
@@ -0,0 +1,90 @@
+# Byte sizes
+
+## musicheader
+* index + channel count: 1
+* address: 2
+* total: 3
+
+## note
+* pitch + length: 1
+* total: 1
+
+## octave
+* total: 1
+
+## notetype
+* length: 1
+* volume + fade: 1
+    * not always present
+* total: 2 to 3
+
+## transpose
+* num octaves + num pitches: 1
+* total 2
+
+## tempo
+* tempo: 1
+* total: 2
+
+## dutycycle
+* square type: 1
+* total 2
+
+## intensity
+* volume + fade: 1
+* total: 2
+
+## slidepitchto
+* duration: 1
+* octave + pitch: 1
+* total: 3
+
+## vibrato
+* delay: 1
+* extent + rate: 1
+* total: 3
+
+## togglenoise
+* drum kit: 1
+* total: 2
+
+## volume
+* volume: 1
+* total: 2
+
+## tone
+* adjust frequency: 1
+* total: 2
+
+## stereopanning
+* left/right: 1
+* total: 2
+
+## tempo_relative
+* adjust tempo: 1
+* total: 2
+
+## setcondition
+* condition: 1
+* total: 2
+
+## jumpif
+* condition: 1
+* address: 2
+* total: 4
+
+## jumpchannel
+* address: 2
+* total: 3
+
+## loopchannel
+* loop count: 1
+* address: 2
+* total: 4
+
+## callchannel
+* address: 2
+* total: 3
+
+## endchannel
+* total: 1
--- optimize.py
+++ optimize.py
@@ -8,7 +8,9 @@ def main():
 
 
 def get_args():
-    parser = argparse.ArgumentParser(description='A script for optimizing pokecrystal music.')
+    parser = argparse.ArgumentParser(
+        description='A script for optimizing pokecrystal compatible '
+                    'music scripts.')
     parser.add_argument('asm', help='script input file')
     parser.add_argument('output', help='optimized output file')
     return parser.parse_args()
@@ -25,6 +27,45 @@ def read_file(filename):
         print(f'No permission to read file {filename}.')
     
     return ()
+
+
+def calc_song_size(song):
+    pass
+
+
+def get_root_command(raw_command):
+    command = raw_command.split(' ')[0].strip()
+    if command == 'notetype':
+        if len(raw_command.split(' ')) > 2:
+            return 'notetype_1'
+        else:
+            return 'notetype_2'
+
+    return command
+
+
+def get_command_size(root_command):
+    """
+    Get a byte size (int) for an associated music command.
+    Size map is ordered by most to least common commands.
+    """
+    # notetype can vary in size
+    size_map_names = ('note', 'octave', 'notetype_1', 'notetype_2',
+                      'dutycycle', 'intensity', 'tempo', 'tone',
+                      'stereopanning', 'vibrato', 'slidepitchto', 'transpose',
+                      'jumpchannel', 'callchannel', 'loopchannel',
+                      'endchannel', 'jumpif', 'setcondition', 'togglenoise',
+                      'volume', 'musicheader')
+    size_map_bytes = (1, 1, 2, 3,
+                      2, 2, 2, 2,
+                      2, 3, 3, 2,
+                      3, 3, 4,
+                      1, 4, 2, 2,
+                      2, 3)
+    
+    bytesize = size_map_bytes[size_map_names.index(root_command)]
+
+    return bytesize
 
 
 if __name__ == '__main__':
