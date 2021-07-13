de8f9273130799b563561ec05221ec3d0f68cf20
Hyperdriveguy <hyperdriveguy@gmail.com>
2021-07-13 11:54:29 -0700

    add file write

--- museoptimize.py
+++ museoptimize.py
@@ -18,16 +18,17 @@ def main():
     print('BEFORE:',pre_optimized_size)
     callchannel_optimized = optimize_callchannel(scrubbed_song)
     post_optimized_size = calc_song_size(callchannel_optimized)
-    print('AFTER:', post_optimized_size)
-    percent_saved_new = (post_optimized_size / pre_optimized_size) * 100
-    print(f'{percent_saved_new:.2f}% of previous size')
+    print('AFTER CALLCHANNEL:', post_optimized_size)
+    percent_saved_callchannel = (post_optimized_size / pre_optimized_size) * 100
+    print(f'{percent_saved_callchannel:.2f}% of previous size')
     old_optimized = read_file('paradise_optimized_v1.asm')
     old_song_size = calc_song_size(old_optimized)
     print('OLD:', old_song_size)
     percent_saved_old = (old_song_size / pre_optimized_size) * 100
     print(f'{percent_saved_old:.2f}% of previous size')
-    percent_diff = percent_saved_old - percent_saved_new
+    percent_diff = percent_saved_old - percent_saved_callchannel
     print(f'\n{percent_diff:.2f}% smaller than old optimizations')
+    write_file('paradise_new_optimum.asm', callchannel_optimized)
 
 
 def get_args():
@@ -50,6 +51,19 @@ def read_file(filename):
         print(f'No permission to read file "{filename}".')
 
     return ()
+
+
+def writelines(input_file, iterable):
+    for line in iterable:
+        input_file.write(line)
+
+
+def write_file(filename, file_contents):
+    try:
+        with open(filename, 'w') as input_file:
+            writelines(input_file, file_contents)
+    except PermissionError:
+        print(f'No permission to write file "{filename}".')
 
 
 if __name__ == '__main__':
