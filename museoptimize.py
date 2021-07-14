3646c260cf5fa3a62e0d04540b28deed99ecc99b
Hyperdriveguy <hyperdriveguy@gmail.com>
2021-07-14 11:04:50 -0700

    Finalize program testing

--- museoptimize.py
+++ museoptimize.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python
 import argparse
-import concurrent.futures
-from ast import Index
 
-from callchannel import optimize_callchannel
-from util_funcs import calc_song_size, scrub_song
+from optimization_passes import run_optimization_passes
+from util_funcs import scrub_song
 
 
 def main():
@@ -14,21 +12,9 @@ def main():
     if input_file == ():
         return
     scrubbed_song = scrub_song(input_file)
-    pre_optimized_size = calc_song_size(scrubbed_song)
-    print('BEFORE:',pre_optimized_size)
-    callchannel_optimized = optimize_callchannel(scrubbed_song)
-    post_optimized_size = calc_song_size(callchannel_optimized)
-    print('AFTER CALLCHANNEL:', post_optimized_size)
-    percent_saved_callchannel = (post_optimized_size / pre_optimized_size) * 100
-    print(f'{percent_saved_callchannel:.2f}% of previous size')
-    old_optimized = read_file('paradise_optimized_v1.asm')
-    old_song_size = calc_song_size(old_optimized)
-    print('OLD:', old_song_size)
-    percent_saved_old = (old_song_size / pre_optimized_size) * 100
-    print(f'{percent_saved_old:.2f}% of previous size')
-    percent_diff = percent_saved_old - percent_saved_callchannel
-    print(f'\n{percent_diff:.2f}% smaller than old optimizations')
-    write_file('paradise_new_optimum.asm', callchannel_optimized)
+    optimized_song, orignal_size, optimum_size = run_optimization_passes(scrubbed_song)
+    print(format_size_diff(orignal_size, optimum_size))
+    write_file('paradise_new_optimum', optimized_song)
 
 
 def get_args():
@@ -60,6 +46,14 @@ def write_file(filename, file_contents):
                 input_file.write(line)
     except PermissionError:
         print(f'No permission to write file "{filename}".')
+
+
+def format_size_diff(old_size, new_size):
+    old_size_format = f'Before optimization: {old_size}\n'
+    new_size_format = f'After optimzation: {new_size}\n'
+    percent_diff = (new_size / old_size) * 100
+    percent_diff_format = f'{percent_diff:.2f}% smaller than orignal size'
+    return old_size_format + new_size_format + percent_diff_format
 
 
 if __name__ == '__main__':
